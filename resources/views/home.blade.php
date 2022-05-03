b09e60262ae6fbf2f934b51d0adbda45d6b2db18
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-05-03 15:03:50 +0200

    data passed via web.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -66,7 +66,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Hello World!
+                    {{ $data }}
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = 'Hello World!';
+    return view('home', ["data"=>$data]);
 });
